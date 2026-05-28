e7c5f002d00e7b1522194d83d1d7254240266d88
Joanna Bassey <joannabassey19@gmail.com>
2026-05-28 23:13:21 +0100

    fix: restored next/font/google implementation for fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import { Providers } from "@/components/shared/providers";
 import type { Metadata } from "next";
+import { Afacad, Dancing_Script, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { env } from "@/env/client";
 import { Toaster } from "sonner";
+
+const afacad = Afacad({ subsets: ["latin"], variable: "--font-afacad" });
+const dancing = Dancing_Script({
+  subsets: ["latin"],
+  variable: "--font-dancing",
+});
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
@@ -23,15 +31,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("font-afacad h-full antialiased")}
+      className={cn("font-afacad h-full antialiased", afacad.variable)}
     >
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Afacad:wght@400..700&family=Dancing+Script:wght@400..700&family=Inter:wght@100..900&display=swap" rel="stylesheet" />
-      </head>
       <body
-        className={`flex min-h-full flex-col`}
+        className={`flex min-h-full flex-col ${afacad.variable} ${dancing.variable} ${inter.variable}`}
       >
         <Providers>
           {children}
