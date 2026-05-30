b1bfbc3df1bc6d2d75d3a66de3f59cb67d4b5389
Zainab Umar Ibrahim <xieumar@gmail.com>
2026-05-29 22:25:22 -0800

    Refactor(Layout): Move conditional navbar and footer rendering into layout component.

--- src/app/(external-pages)/layout.tsx
+++ src/app/(external-pages)/layout.tsx
@@ -1,12 +1,30 @@
-import Footer from "@/components/layout/Footer";
-import { Navbar } from "@/components/layout/Navbar";
+"use client";
+
 import React from "react";
+import { usePathname } from "next/navigation";
+import { Navbar } from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 export default function ExternalPagesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const pagesWithLayout = [
+    "/contact",
+    "/faq",
+    "/privacy-policy",
+    "/terms",
+    "/waitlist",
+  ];
+
+  const showLayout = pagesWithLayout.includes(pathname);
+
+  if (!showLayout) {
+    return <>{children}</>;
+  }
+
   return (
     <>
       <Navbar />
